ce715694a74ff7652cdedcfbe3d3eeb7ad1717c7
elenays <helenasp@yandex.ru>
2020-07-24 21:28:31 +0300

    настройка mysql

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const path = require('path')
+const todoRoutes = require('./routes/todo')
+const sequelize = require('./utils/database')
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
-
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/api/todo', todoRoutes)
 
 app.use((req, res, next) => {
     res.sendFile('/index.html')
 })
 
-app.listen(PORT)
+async function start() {
+    try {
+        await sequelize.sync()
+        app.listen(PORT)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start()
---
+++ routes/todo.js
@@ -0,0 +1,24 @@
+const { Router } = require('express')
+
+const router = Router()
+
+// Получение списка задач
+router.get('/', (req, res) => {
+
+})
+
+// Создание новой задачи
+router.post('/', (req, res) => {
+
+})
+
+// Изменение задачи
+router.put('/:id', (req, res) => {
+
+})
+// Удаление задачи
+router.delete('/:id', (req, res) => {
+
+})
+
+module.exports = router
