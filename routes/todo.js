3d9fa36870a4e33a1bbe7a015095ea41e45e0bbb
elenays <helenasp@yandex.ru>
2020-08-02 17:32:09 +0300

    сохранение в базу

--- routes/todo.js
+++ routes/todo.js
@@ -1,24 +1,57 @@
 const { Router } = require('express')
+const Todo = require('../models/todo')
 
 const router = Router()
 
 // Получение списка задач
 router.get('/', (req, res) => {
+    try {
 
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 })
 
 // Создание новой задачи
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+    try {
+        const todo = await Todo.create({
+            title: req.body.title,
+            done: false
+        })
+        res.status(201).json({ todo })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 })
 
 // Изменение задачи
 router.put('/:id', (req, res) => {
+    try {
 
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 })
 // Удаление задачи
 router.delete('/:id', (req, res) => {
+    try {
 
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
 })
 
 module.exports = router
