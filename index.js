7e368a63651148bba902737cd7b2c53ec9893f32
elenays <helenasp@yandex.ru>
2020-08-19 13:05:55 +0300

    установка и вывод простых запросов

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
-const todoRoutes = require('./routes/todo')
+const graphqlHTTP = require('express-graphql').graphqlHTTP
 const sequelize = require('./utils/database')
+const schema = require('./graphql/schema')
+const resolver = require('./graphql/resolver')
 
 const app = express()
 
@@ -9,7 +11,13 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
-app.use('/api/todo', todoRoutes)
+
+app.use(graphqlHTTP({
+    schema: schema,
+    rootValue: resolver,
+    graphiql: true
+}))
+
 
 app.use((req, res, next) => {
     res.sendFile('/index.html')
